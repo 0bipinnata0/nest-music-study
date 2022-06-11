f0cc79fb3eff8e84767f42c5f003f47881e7191f
tom <mail>
2022-06-11 17:42:17 +0800

    music menus

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Divider,
   LinkBox,
   LinkOverlay,
   List,
@@ -8,7 +9,13 @@ import {
 } from "@chakra-ui/layout";
 import NextImage from "next/image";
 import NextLink from "next/link";
-import { MdHome, MdLibraryMusic, MdSearch } from "react-icons/md";
+import {
+  MdFavorite,
+  MdHome,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdSearch,
+} from "react-icons/md";
 
 const navMenu = [
   {
@@ -25,6 +32,19 @@ const navMenu = [
     name: "Your Library",
     icon: MdLibraryMusic,
     route: "/Library",
+  },
+];
+
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorites",
   },
 ];
 
@@ -57,6 +77,23 @@ const SideBar = () => {
             ))}
           </List>
         </Box>
+        <Box marginTop="20px">
+          <List spacing="2">
+            {musicMenu.map(({ name, route, icon }) => (
+              <ListItem paddingX="20px" fontSize="16px" key={name}>
+                <LinkBox>
+                  <NextLink href={route} passHref>
+                    <LinkOverlay>
+                      <ListIcon as={icon} color="white" marginRight="20px" />
+                      {name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" />
       </Box>
     </Box>
   );
