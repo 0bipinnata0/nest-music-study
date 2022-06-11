eb1dc2c84cdd03f7fd261e4e34c68d4bf6af7c13
tom <mail>
2022-06-11 17:22:46 +0800

    添加menus

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,19 +1,62 @@
-import { Box } from "@chakra-ui/layout";
-import Image from "next/image";
+import {
+  Box,
+  LinkBox,
+  LinkOverlay,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/layout";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { MdHome, MdLibraryMusic, MdSearch } from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/Library",
+  },
+];
 
 const SideBar = () => {
   return (
     <Box
-      width="100"
-      height="calc(100vh-100px)"
+      width="100%"
+      height="calc(100vh - 100px)"
       bg="black"
       paddingX="5px"
       color="gray"
     >
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
-          <Image src="/logo.svg" height="60" width="120" />
+          <NextImage src="/logo.svg" height="60" width="120" />
         </Box>
+      </Box>
+      <Box marginBottom="20px">
+        <List>
+          {navMenu.map(({ name, route, icon }) => (
+            <ListItem paddingX="20px" fontSize="16px" key={name}>
+              <LinkBox>
+                <NextLink href={route} passHref>
+                  <LinkOverlay>
+                    <ListIcon as={icon} color="white" marginRight="20px" />
+                    {name}
+                  </LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Box>
   );
