c1842a45e473ec9aa6589cb349a47f79841a3856
tom <mail>
2022-06-11 23:50:50 +0800

    创建用户注册的api

---
+++ pages/api/signup.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
+const signup = async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user.id,
+        time: Date.now(),
+      },
+      "hello",
+      {
+        expiresIn: "8h",
+      }
+    );
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        // means https only
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+
+    res.json(user);
+  } catch (e) {
+    res.status(401);
+    res.json({ error: "User already exists" });
+  }
+};
+
+export default signup;
